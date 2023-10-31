17545e55c4815e1d5784d7757404b6e0a8555064
Mukul Anand <mukulanand758@gmail.com>
2023-10-31 20:35:49 +0530

    menu_navigation_drawer_complete

--- app/src/main/java/com/example/gmailapp/ui/social/SocialViewModel.kt
+++ app/src/main/java/com/example/gmailapp/ui/social/SocialViewModel.kt
@@ -1,13 +1,12 @@
-package com.example.gmailapp.ui.home
+package com.example.gmailapp.ui.social
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
-
+class SocialViewModel : ViewModel() {
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is Primary Fragment"
     }
     val text: LiveData<String> = _text
-}
+    }
